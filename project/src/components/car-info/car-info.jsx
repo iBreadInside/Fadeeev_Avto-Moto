54f319468d2a6de66f5158cfbbcb193565c5153e
Владимир <nekro199107@gmail.com>
2021-10-21 23:39:10 +0300

    Добавил верстку основного контента

---
+++ project/src/components/car-info/car-info.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from './car-info.module.scss';
+
+const Car = {
+  name: 'Марпех 11',
+  details: {
+    transmission: 'Роботизированная',
+    power: 249,
+    fuel: 'Бензиновый',
+    drive: 'Полный',
+    volume: 2.4,
+    torque: '370/4500',
+    cyl: 4,
+  },
+  fullPrice: '2 400 000',
+  currentPrice: '2 300 000',
+  credit: '11 000',
+};
+
+export default function CarInfo() {
+  return(
+    <section className='car'>
+      <h1 className={styles.content}>Информация об автомобиле</h1>
+
+      <div className='slider'>
+
+      </div>
+
+      <div className='info'>
+        <h2 className={styles.name}>{Car.name}</h2>
+
+        <ul className={styles.details}>
+          <li className={styles.details__item}>бензин</li>
+          <li className={`${styles.details__item} ${styles.gearbox}`}>механика</li>
+          <li className={`${styles.details__item} ${styles.power}`}>100 л.с.</li>
+          <li className={`${styles.details__item} ${styles.volume}`}>1.4 л</li>
+        </ul>
+
+        <ul className={styles.prices}>
+          <li className='prices__current'>{Car.currentPrice} &#8381;</li>
+          <li className='prices__full'>{Car.fullPrice} &#8381;</li>
+        </ul>
+
+        <div className={styles.buttons}>
+          <button className='request'>оставить заявку</button>
+          <button className='credit'>в кредит от {Car.credit} &#8381;</button>
+        </div>
+      </div>
+    </section>
+  );
+}
